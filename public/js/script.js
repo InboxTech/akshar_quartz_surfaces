77b5260f89574550334069f765a300befa33f329
Krishna Sheth <krishna.sheth@inbox-infotech.com>
2024-11-11 10:52:02 +0530

    remove 'the' before the company name and put slider in autoplay

--- public/js/script.js
+++ public/js/script.js
@@ -35,6 +35,10 @@ var swiper = new Swiper(".myBannerSwiper", {
     hashNavigation: {
       watchState: true,
     },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
